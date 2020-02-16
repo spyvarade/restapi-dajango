ff36ef24d3244c6604a29e6ba223a31237d53974
Spyvarade <spyvarade@gmail.com>
2020-02-17 00:20:18 +0530

    single article fecth

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, aritcle_detail
 
 urlpatterns = [
     path('article/',article_list),
+    path('detail/<int:pk>/',aritcle_detail)
+
 ]
--- api/views.py
+++ api/views.py
@@ -27,3 +27,30 @@ def article_list(request):
 			return JsonResponse(serializer.data, status=201)
 
 		return JsonResponse(serializer.errors, status=400)
+
+
+def aritcle_detail(request, pk):
+	try:
+		article =Article.objects.get(pk=pk)
+
+	except Article.DoseNotExist:
+		return HttpResponse(status=400)
+
+	if request.method == 'GET':
+		serializer = ArticleSerializer(article)
+
+		return JsonResponse(serializer.data)
+
+	elif request.method == "PUT":
+		data = JSONParser().parse(request)
+		serializer = ArticleSerializer(article, data=data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=400)
+
+	elif request.method == "DELETE":
+		article.delete()
+		return HttpResponse(status=204)
+
